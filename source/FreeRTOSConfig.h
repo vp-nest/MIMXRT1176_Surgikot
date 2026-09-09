2685bea60614d18e11d6e9497ec40e10850810c8
Jithin <jithinramesh6@gmail.com>
2026-09-09 13:25:00 +0530

    Update project and FreeRTOS configuration

--- source/FreeRTOSConfig.h
+++ source/FreeRTOSConfig.h
@@ -50,7 +50,7 @@
 #define configUSE_PREEMPTION                    1
 #define configUSE_TICKLESS_IDLE                 0
 #define configCPU_CLOCK_HZ                      (SystemCoreClock)
-#define configTICK_RATE_HZ                      ((TickType_t)200)
+#define configTICK_RATE_HZ                      ((TickType_t)1000)//old 200
 #define configMAX_PRIORITIES                    10
 #define configMINIMAL_STACK_SIZE                ((unsigned short)128 * 4)
 #define configMAX_TASK_NAME_LEN                 20
@@ -82,7 +82,7 @@
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     0
-#define configUSE_TICK_HOOK                     0
+#define configUSE_TICK_HOOK                     1 //old 0
 #define configCHECK_FOR_STACK_OVERFLOW          0
 #define configUSE_MALLOC_FAILED_HOOK            0
 #define configUSE_DAEMON_TASK_STARTUP_HOOK      0
